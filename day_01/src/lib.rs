98049500cf4e9503698c8cff6069a80d508d2036
Miguel Mendes <miguelmendeseiriz@gmail.com>
2022-12-01 11:32:45 +0000

    added part two day one

--- day_01/src/lib.rs
+++ day_01/src/lib.rs
@@ -11,14 +11,33 @@ pub fn day_one_part_one(path: &str) -> Result<u64, Error> {
 
             total_calories
         })
-        .max();
+        .max()
+        .unwrap_or(0);
 
-    Ok(max.unwrap_or(0))
+    Ok(max)
+}
+
+pub fn day_one_part_two(path: &str) -> Result<u64, Error> {
+    let mut max: Vec<u64> = fs::read_to_string(path)?
+        .split_terminator("\n\n")
+        .map(|user_block| {
+            let total_calories: u64 = user_block
+                .split_terminator('\n')
+                .filter_map(|calories| calories.parse::<u64>().ok())
+                .sum();
+
+            total_calories
+        })
+        .collect();
+
+    max.sort();
+
+    Ok(max.into_iter().rev().take(3).sum())
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day_one_part_one;
+    use crate::{day_one_part_one, day_one_part_two};
 
     #[test]
     fn day_one_part_one_example() {
@@ -30,5 +49,17 @@ mod tests {
     fn day_one_part_one_data() {
         let result = day_one_part_one("data.txt").unwrap();
         assert_eq!(result, 72511);
+    }
+
+    #[test]
+    fn day_one_part_two_example() {
+        let result = day_one_part_two("example.txt").unwrap();
+        assert_eq!(result, 45000);
+    }
+
+    #[test]
+    fn day_one_part_two_data() {
+        let result = day_one_part_two("data.txt").unwrap();
+        assert_eq!(result, 212117);
     }
 }
