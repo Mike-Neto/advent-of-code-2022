19491ca5059ec35486eb2dd774716926a4ad5400
Miguel Mendes <miguelmendeseiriz@gmail.com>
2022-12-02 11:45:09 +0000

    day02 part two

--- day_02/src/lib.rs
+++ day_02/src/lib.rs
@@ -35,9 +35,32 @@ impl Choice {
             },
         }
     }
+
+    fn play_against_2(&self, outcome: &Self) -> u64 {
+        // Choice::Rock Lose!
+        // Choice::Paper Draw!
+        // Choice::Scissors Win!
+        match outcome {
+            Choice::Rock => match self {
+                Choice::Rock => 3,
+                Choice::Paper => 1,
+                Choice::Scissors => 2,
+            },
+            Choice::Paper => match self {
+                Choice::Paper => 5,
+                Choice::Scissors => 6,
+                Choice::Rock => 4,
+            },
+            Choice::Scissors => match self {
+                Choice::Scissors => 7,
+                Choice::Paper => 9,
+                Choice::Rock => 8,
+            },
+        }
+    }
 }
 
-pub fn day_one_part_one(path: &str) -> Result<u64, Error> {
+pub fn day_two_part_one(path: &str) -> Result<u64, Error> {
     let score: u64 = fs::read_to_string(path)?
         .lines()
         .map(|round| {
@@ -53,20 +76,49 @@ pub fn day_one_part_one(path: &str) -> Result<u64, Error> {
     Ok(score)
 }
 
+pub fn day_two_part_two(path: &str) -> Result<u64, Error> {
+    let score: u64 = fs::read_to_string(path)?
+        .lines()
+        .map(|round| {
+            round
+                .split_whitespace()
+                .take(2)
+                .filter_map(|char| Choice::from_char(char))
+                .collect()
+        })
+        .map(|round: Vec<Choice>| round[0].play_against_2(&round[1]))
+        .sum();
+
+    Ok(score)
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::day_one_part_one;
+    use crate::{day_two_part_one, day_two_part_two};
 
     #[test]
-    fn day_one_part_one_example() {
-        let result = day_one_part_one("example.txt").unwrap();
+    fn day_two_part_one_example() {
+        let result = day_two_part_one("example.txt").unwrap();
         assert_eq!(result, 15);
     }
 
     #[test]
-    fn day_one_part_one_data() {
-        let result = day_one_part_one("data.txt").unwrap();
+    fn day_two_part_one_data() {
+        let result = day_two_part_one("data.txt").unwrap();
         assert!(result < 12149);
         assert_eq!(result, 11603);
+    }
+
+    #[test]
+    fn day_two_part_two_example() {
+        let result = day_two_part_two("example.txt").unwrap();
+        assert_eq!(result, 12);
+    }
+
+    #[test]
+    fn day_two_part_two_data() {
+        let result = day_two_part_two("data.txt").unwrap();
+        assert!(result > 11915);
+        assert_eq!(result, 12);
     }
 }
